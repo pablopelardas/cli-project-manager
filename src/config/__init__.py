a40562affb0b06da82f54f120a21178d8f9e5974
Pablo Pelardas <pablo.pelardas@gmail.com>
2024-02-08 21:31:31 -0300

    Finish with basic tools (add, rm, list and open projects and combos)

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -9,6 +9,7 @@ class Config:
     def __init__(self):
         self._app_name = "LocalProjectManager"
         self._app_config_dir = platformdirs.user_config_dir(self._app_name)
+        print(self._app_config_dir)
         self._app_config_file = os.path.join(self._app_config_dir, "config.json")
         self._config = {}
         self._commands = []
@@ -27,9 +28,9 @@ class Config:
         if not os.path.exists(self._app_config_file):
             with open(self._app_config_file, "w") as f:
                 default_config = {
-                    "project_paths": [
-                        "~/working/ypf/projects"
-                    ],                      
+                    "projects": [
+                    ],
+                    "combos": []         
                 }
                 # dict to json
                 default_config = json.dumps(default_config, indent=4)
@@ -37,43 +38,90 @@ class Config:
     def _load_config(self):
         with open(self._app_config_file, "r") as f:
             self._config = json.load(f)
+            if "projects" not in self._config:
+                self._config["projects"] = []
+            if "combos" not in self._config:
+                self._config["combos"] = []
     def _save_config(self):
         with open(self._app_config_file, "w") as f:
             f.write(json.dumps(self._config, indent=4))
-    def add_project_path(self, path):
+    def add_project_path(self, project):
+        for saved_project in self._config["projects"]:
+            if project["path"] == saved_project["path"]:
+                raise ValueError(f"Project '{project['path']}' already in list")
+            if project["alias"] == saved_project["alias"]:
+                raise ValueError(f"Alias '{project['alias']}' already in list")
         # verify if path exists
-        if not os.path.exists(path):
-            raise ValueError(f"Path '{path}' does not exist")
-        if path in self._config["project_paths"]:
-            raise ValueError(f"Path '{path}' already in list")
+        if not os.path.exists(project["path"]):
+            raise ValueError(f"Path '{project['path']}' does not exist")
         # add path to config
-        self._config["project_paths"].append(path)
+        self._config["projects"].append(project)
         self._save_config()
-    def remove_project_path(self, path):
+    def remove_project_path(self, project):
         # verify if path exists
-        if not os.path.exists(path):
-            raise ValueError(f"Path '{path}' does not exist")
-        if path not in self._config["project_paths"]:
-            raise ValueError(f"Path '{path}' not in list")
-        # remove path from config
-        self._config["project_paths"].remove(path)
+        for saved_project in self._config["projects"]:
+            if project["path"] == saved_project["path"] or project["alias"] == saved_project["alias"]:
+                # remove path from config
+                self._config["projects"].remove(saved_project)
+                self._save_config()
+                return
+        raise ValueError(f"Project '{project['path']}' - Alias '{project['alias']}' not in list")
+    def modify_project(self, project, alias):
+        for saved_project in self._config["projects"]:
+            if project["path"] == saved_project["path"] or project["alias"] == saved_project["alias"]:
+                saved_project["alias"] = alias
+                self._save_config()
+                return
+        raise ValueError(f"Project '{project['path']}' not in list")
+    def add_combo(self, combo):
+        for saved_combo in self._config["combos"]:
+            if combo["name"] == saved_combo["name"]:
+                raise ValueError(f"Combo '{combo['name']}' already in list")
+        self._config["combos"].append(combo)
         self._save_config()
-    def get_project_paths(self):
-        return self._config["project_paths"]
+    def remove_combo(self, combo_name):
+        found = False
+        for combo in self._config["combos"]:
+            if combo_name == combo.get("name"):
+                self._config["combos"].remove(combo)
+                self._save_config()
+                found = True
+                return
+        if not found:
+            raise ValueError(f"Combo '{combo_name}' not in list")
+    def get_projects(self):
+        return self._config["projects"]
+    def get_combos(self):
+        return self._config["combos"]
+    def get_combo_projects(self, combo_name):
+        for combo in self._config["combos"]:
+            if combo_name == combo.get("name"):
+                return combo.get("projects")
+        raise ValueError(f"Combo '{combo_name}' not in list")
+    def get_project(self, alias):
+        for project in self._config["projects"]:
+            if alias == project.get("alias"):
+                return project
+        raise ValueError(f"Project '{alias}' not in list")
 def initialize_commands(shell, config):
     
     commands = [
         Command("exit", lambda: fn_exit(shell)),
         Command(":q", lambda: fn_exit(shell)),
         Command("ls", lambda : fn_ls(config)),
+        Command("add", lambda *args: fn_add_project(config)),
+        Command("rm", lambda *args: fn_remove_project(config)),
+        Command("open", lambda: open_project(config)),
+        Command("lsc", lambda: fn_ls_combo(config)),
+        Command("addc", lambda: fn_add_combo(config)),
+        Command("rmc", lambda: fn_remove_combo(config)),
+        Command("openc", lambda: fn_open_combo(config)),
         Command("config", lambda: fn_config(config)),
-        Command("add", lambda *args: fn_add_project_path(config, *args)),
-        Command("rm", lambda *args: fn_remove_project_path(config, *args)),
         Command("clear", lambda: fn_clear()),
-        Command("open", lambda num: open_project(num, config)),
-        Command("gui", lambda: open_gui(config)),
-        Command("help", lambda: fn_welcome())
+        Command("help", lambda: fn_welcome()),
+        Command("mod", lambda *args: fn_modify_project(config))
     ]
+    config._commands = commands
     for command in commands:
         shell.add_command(command.name, command.func)
     return shell
--- src/config/commands.py
+++ src/config/commands.py
@@ -1,5 +1,4 @@
 from pytermgui import tim, palette
-from widgets import example
 from multiprocessing import Process
 import os
 
@@ -8,87 +7,144 @@ palette.regenerate(primary="mediumpurple")
 def fn_exit(shell):
     shell.stop()
 def fn_ls(config):
-    project_paths = config.get_project_paths()
-    for i in range(len(project_paths)):
-        print(f"{i+1}. {project_paths[i]}")
+    projects = config.get_projects()
+    for project in projects:
+        tim.print(f"[primary+3] [bold secondary]{project['alias']}[/] - [tertiary]{project['path']}")
 def fn_config(config):
     print(config._config)
-def fn_add_project_path(config, *args):
-    if len(args) == 0:
+def fn_add_project(config):
         try:
-            input_path = input("Enter the path to add (pwd to add current path): ")
-            handle_input_path(input_path, config.add_project_path)
+            input_path = input("Enter the path of the project to add (pwd to add current path): ")
+            alias = input("Enter the alias for the project: ")
+            handle_input_path(input_path, config.add_project_path, alias)
         except ValueError as e:
             print(e)
-    else:
-        handle_path_args(*args, fn=config.add_project_path)
 
-def fn_remove_project_path(config, *args):
-    if len(args) == 0:
+def fn_remove_project(config):
         try:
-            input_path = input("Enter the path to remove (pwd to remove current path): ")
-            handle_input_path(input_path, config.remove_project_path)
+            input_path = input("Enter the path or alias of the project to remove: ")
+            handle_input_path(input_path, config.remove_project_path, input_path)
         except ValueError as e:
             print(e)
-    else:
-        handle_path_args(*args, fn=config.remove_project_path)
-# 150
+def fn_modify_project(config):
+    try:
+        input_path = input("Enter the path or alias of the project to modify: ")
+        alias = input("Enter the new alias for the project: ")
+        def func(project):
+            config.modify_project(project, alias)
+        handle_input_path(input_path, func, input_path)
+    except ValueError as e:
+        print(e)
+def fn_add_combo(config):
+    try:
+        combo = input("Enter the name of the combo to add: ")
+        fn_ls(config)
+        alias = input("Enter the alias of the project to add to the combo (separated by space): ")
+        combo_obj = {"name": combo, "projects": alias.split(" ")}
+        config.add_combo(combo_obj)
+    except ValueError as e:
+        print(e)
+def fn_remove_combo(config):
+    try:
+        combo = input("Enter the name of the combo to remove: ")
+        config.remove_combo(combo)
+    except ValueError as e:
+        print(e)
+def fn_open_combo(config):
+    try:
+        combo = input("Enter the name of the combo to open: ")
+        config.get_combo_projects(combo)
+        for project in config.get_combo_projects(combo):
+            p = config.get_project(project)
+            os.system(f"code {p["path"]}")
+    except ValueError as e:
+        print(e)
+def fn_ls_combo(config):
+    combos = config.get_combos()
+    for combo in combos:
+        tim.print(f"[primary+3] [bold secondary]{combo['name']}[/] - [tertiary]{' '.join(combo['projects'])}")
+def fn_mod_combo(config):
+    try:
+        fn_ls_combo(config)
+        combo = input("Enter the name of the combo to modify: ")
+        alias = input("Enter the new alias for the combo: ")
+        config.modify_combo(combo, alias)
+    except ValueError as e:
+        print(e)
+
 def fn_welcome():
     tim.print("[primary+3]Welcome to [!gradient(150) italic]LocalProjectManager!")
+    tim.print("[primary+3]This is a simple tool to manage your local projects and open them with a single command")
+# divide general from projects and combo commands
+    print("")
+    tim.print("[secondary]===[primary+3]General[secondary]===[primary+3]")
+    print("")
     tim.print("[primary+3]Type [bold firebrick]'exit'[/][primary+3] to exit the program")
-    tim.print("[primary+3]Type [bold rosybrown]'config'[/][primary+3] to read the config file")
+    tim.print("[primary+3]Type [bold darkslateblue]'help'[/][primary+3] to read again this message")
+    tim.print("[primary+3]Type [bold darkorange]'clear'[/][primary+3] to clear the terminal")
+
+    print("")
+    tim.print("[secondary]===[primary+3]Projects[secondary]===[primary+3]")
+    print("")
+    tim.print("[primary+3]Type [bold primary]'ls'[/][primary+3] to list the projects")
     tim.print("[primary+3]Type [bold royalblue]'add'[/][primary+3] to add a project to the list")
     tim.print("[primary+3]Type [bold sienna]'rm'[/][primary+3] to remove a project from the list")
-    tim.print("[primary+3]Type [bold primary]'ls'[/][primary+3] to list the projects")
-    tim.print("[primary+3]Type [bold darkslateblue]'help'[/][primary+3] to read again this message")
+    tim.print("[primary+3]Type [bold darkorange]'open'[/][primary+3] to open a project")
+
+    print("")
+    tim.print("[secondary]===[primary+3]Combos[secondary]===[primary+3]")
+    print("")
+    tim.print("[primary+3]Type [bold primary]'lsc'[/][primary+3] to list the combos")
+    tim.print("[primary+3]Type [bold royalblue]'addc'[/][primary+3] to add a combo to the list")
+    tim.print("[primary+3]Type [bold sienna]'rmc'[/][primary+3] to remove a combo from the list")
+    tim.print("[primary+3]Type [bold darkorange]'openc'[/][primary+3] to open a combo")
+    print("")
+    print("")
 
 def fn_clear():
     print("\033c")
 
-def open_project(num, config):
+def open_project(config):
     try:
-        num = int(num)
-        project_paths = config.get_project_paths()
-        if num < 1 or num > len(project_paths):
-            print(f"Project number must be between 1 and {len(project_paths)}")
-            return
-        p = Process(target=os.system, args=(f"code {project_paths[num-1]}",))
-        p.start()
-        config._processes.append(p)
-    except ValueError:
-        print("Project number must be an integer")
-
-def open_gui(config):
-    # run example in a new process to avoid blocking the shell
-    try:
-        p = Process(target=os.system, args=("python3 ./widgets/example.py",))
-        p.start()
-        config._processes.append(p)
-    except Exception as e:
-        print(f"An error occurred: {e}")
         
+        fn_ls(config)
+        alias = input("Enter the alias of the project to open: ")
+        project = config.get_project(alias)
+        # find the project
+        if not project:
+            raise ValueError(f"Project '{alias}' not in list")
+        os.system(f"code {project['path']}")
+    except Exception as e:
+        print(e)
+   
 def handle_path_args(*args, fn):
-    for path in args:
+    for arg in args:
         try:
-            if path == "pwd" or path == '.':
+            if arg == "pwd" or arg == '.':
                 path = os.getcwd()
-            elif path[0] != "/":
-                if path[0] == "~":
-                    path = os.path.expanduser(path)
-                elif path[0] == ".":
-                    path = os.path.join(os.getcwd(), path[1:])
+            elif arg[0] != "/":
+                if arg[0] == "~":
+                    path = os.path.expanduser(arg)
+                elif arg[0] == ".":
+                    path = os.path.join(os.getcwd(), arg[1:])
                 else:
-                    path = os.path.join(os.getcwd(), path)
+                    path = os.path.join(os.getcwd(), arg)
             fn(path)
         except ValueError as e:
             print(e)
 
-def handle_input_path(path, fn):
+def handle_input_path(path, fn, alias= None):
     try:
-        if path == "pwd":
+        if path == "pwd" or path == '.':
             path = os.getcwd()
         if path[0] != "/":
-            path = os.path.join(os.getcwd(), path)
-        fn(path)
+            if path[0] == "~":
+                path = os.path.expanduser(path)
+            elif path[0] == ".":
+                path = os.path.join(os.getcwd(), path[1:])
+            else:
+                path = os.path.join(os.getcwd(), path)
+        project = {"path": path, "alias": alias if alias else path.split("/")[-1]}
+        fn(project)
     except ValueError as e:
         print(e)
